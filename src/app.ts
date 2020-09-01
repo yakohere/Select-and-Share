51e5a0ee121b465fa4ca1b369ec16b149bd12273
yakohere <yaxyo.ismoiljonov@teamuni.uz>
2020-09-01 23:49:52 +0500

    showing fetched coordinates in the map

--- src/app.ts
+++ src/app.ts
@@ -7,18 +7,39 @@ const adressInput = (document.getElementById(
 
 const GOOGLE_API_KEY = process.env.GOOGLE_API_KEY;
 
+declare var google: any;
+
 function searchAdressHandler(event: Event) {
   event.preventDefault();
   const enteredAdress = adressInput.value;
 
   axios
-    .get(
+    .get<{
+      results: { geometry: { location: { lat: number; lng: number } } }[];
+      status: "OK" | "ZERO RESULTS";
+    }>(
       `https://maps.googleapis.com/maps/api/geocode/json?address=${encodeURI(
         enteredAdress
       )}&key=${GOOGLE_API_KEY}`
     )
-    .then((res) => console.log(res))
-    .catch((err) => console.log(err));
+    .then((res) => {
+      if (res.data.status !== "OK") {
+        throw new Error("Could not fetch location!");
+      }
+      const coordinates = res.data.results[0].geometry.location;
+      const map = new google.maps.Map(
+        document.getElementById("map") as HTMLElement,
+        {
+          center: coordinates,
+          zoom: 5,
+        }
+      );
+      new google.maps.Marker({ position: coordinates, map: map });
+    })
+    .catch((err) => {
+      alert(err.message);
+      console.log(err);
+    });
 }
 
 form.addEventListener("submit", searchAdressHandler);
