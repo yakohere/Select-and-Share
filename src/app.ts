5acaa964910ac81f8c816c6eac7cd78efbb7d16f
yakohere <yaxyo.ismoiljonov@teamuni.uz>
2020-09-01 22:58:31 +0500

    update

--- src/app.ts
+++ src/app.ts
@@ -1,9 +1,19 @@
 const form = document.querySelector("form")!;
-// const adressInput
+import * as dotenv from "dotenv";
 
+const adressInput = (document.getElementById(
+  "address"
+)! as unknown) as HTMLInputElement;
+
+dotenv.config();
+
+const GOOGLE_API_KEY = process.env.GOOGLE_API_KEY;
 
 function searchAdressHandler(event: Event) {
-  event.preventDefault();   
+  event.preventDefault();
+  const enteredAdress = adressInput.value;
+
+  // url = https://maps.googleapis.com/maps/api/geocode/json?address=1600+Amphitheatre+Parkway, +Mountain+View,+CA&key=GOOGLE_API_KEY
 }
 
 form.addEventListener("submit", searchAdressHandler);
