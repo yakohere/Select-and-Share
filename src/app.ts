327531f5b49fbc4a879d8717ea666588d74c8b2a
yakohere <yaxyo.ismoiljonov@teamuni.uz>
2020-09-01 20:38:06 +0500

    update

--- src/app.ts
+++ src/app.ts
@@ -1,1 +1,9 @@
-// Code goes here!
+const form = document.querySelector("form")!;
+// const adressInput
+
+
+function searchAdressHandler(event: Event) {
+  event.preventDefault();   
+}
+
+form.addEventListener("submit", searchAdressHandler);
